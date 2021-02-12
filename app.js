97d9428777981cbd02206df678282ca7fd7d7ed3
Perlerinette <73797606+Perlerinette@users.noreply.github.com>
2021-02-12 17:17:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-let express = require("express");
-let app = express();
-// const sequelize = require("./db");
-let product = require("./controllers/productController");
-=======
 require("dotenv").config();
 let express = require('express');
 let app = express();
 let sequelize = require('./db');
 
+let product = require("./controllers/productController");
 let user = require('./controllers/userController');
 
 sequelize.sync();
@@ -17,15 +12,8 @@ sequelize.sync();
 app.use(require('./middleware/headers'));
 app.use(express.json());
 
-/****
- *  EXPOSED route *
- ****/
+
 app.use('/user', user);
-
->>>>>>> 72668eb4489dd94c4773204ede3abc23913d0d66
-
-// sequelize.sync();
-
 app.use("/product", product);
 
 app.listen(3000, function () {
